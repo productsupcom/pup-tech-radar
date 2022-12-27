be8673f57e43dd23b5c3a5411ce61bb90dafa184
Carsten Neuendorf <carsten.neuendorf@productsup.com>
2022-12-27 14:39:31 +0100

    techradar update based on the meeting notes from our quarterly checkin: https://productsup.atlassian.net/wiki/spaces/EN/pages/2822439039/2022-12-22+-+Tech+Radar?focusedTaskId=1

--- docs/radar.js
+++ docs/radar.js
@@ -280,7 +280,7 @@ function radar_visualization(config) {
 
     // footer
     radar.append("text")
-      .attr("transform", translate(footer_offset.x, (footer_offset.y+40)))
+      .attr("transform", translate(footer_offset.x, (footer_offset.y+60)))
       .text("▲ moved up     ▼ moved down")
       .attr("xml:space", "preserve")
       .style("font-family", "Arial, Helvetica")
